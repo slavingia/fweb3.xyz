d0b4242623276f4899a9904ee543aaa56279881b
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-01-20 08:30:03 -0800

    cp

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home() {
         <meta content="Learn and build Web3 in Fweb3" name="description" />
         <meta content="Fweb3" property="og:title" />
         <meta content="Learn and build Web3 in Fweb3" property="og:description" />
-        <meta content="/fweb3.png" property="og:image" />
+        <meta content="https://fweb3.xyz/fweb3.png" property="og:image" />
         <meta content="Learn and build Web3 in Fweb3" property="twitter:title" />
         <meta content="Learn and build Web3 in Fweb3" property="twitter:description" />
-        <meta content="/fweb3.png" property="twitter:image" />
+        <meta content="https://fweb3.xyz/fweb3.png" property="twitter:image" />
         <meta property="og:type" content="website" />
         <meta content="summary_large_image" name="twitter:card" />
       </Head>
