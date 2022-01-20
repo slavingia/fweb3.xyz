639f774b11240a16f2b0cebb6ab05530cbf1f293
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-01-20 08:27:55 -0800

    cp

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,15 @@ export default function Home() {
         <title>Fweb3</title>
         <meta name="description" content="Learn and build Web3 in Fweb3" />
         <link rel="icon" href="/icon.png" />
+        <meta content="Learn and build Web3 in Fweb3" name="description" />
+        <meta content="Fweb3" property="og:title" />
+        <meta content="Learn and build Web3 in Fweb3" property="og:description" />
+        <meta content="/fweb3.png" property="og:image" />
+        <meta content="Learn and build Web3 in Fweb3" property="twitter:title" />
+        <meta content="Learn and build Web3 in Fweb3" property="twitter:description" />
+        <meta content="/fweb3.png" property="twitter:image" />
+        <meta property="og:type" content="website" />
+        <meta content="summary_large_image" name="twitter:card" />
       </Head>
 
       <main className={styles.main}>
