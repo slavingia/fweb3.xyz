85c232481a76f4af770f06c86a69d23c1669eac7
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-01-20 10:53:51 -0800

    cp

---
+++ fweb3.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts@4.4.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.4.2/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Fweb3 is ERC20, ERC20Burnable {
+    constructor() ERC20("Fweb3", "FWEB3") {
+        _mint(msg.sender, 10000000 * 10 ** decimals());
+    }
+}
