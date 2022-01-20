3ec26694d350c2bb292c2dce600925ba90dbd181
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-01-20 09:31:41 -0800

    cp

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <a href="https://forms.gle/CAmcLmTd6zwoaroy6">
+        <a href="https://discord.gg/wwXbpv2HAj">
           <h1 className={styles.title}>
             <Image src="/fweb3.svg" width="200" height="41" />
           </h1>
