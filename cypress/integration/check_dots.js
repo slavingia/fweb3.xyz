13ca46e14578b985d18a67469dbbaa9d1c8d877b
Slin Lee <slinlee@gmail.com>
2022-02-20 13:14:30 -0800

    Apply prettier

--- cypress/integration/check_dots.js
+++ cypress/integration/check_dots.js
@@ -6,10 +6,9 @@
 // https://on.cypress.io/writing-first-test
 
 describe("Dots Tests", () => {
-    it("Loads the page", () => {
-        cy.visit("/");
-        cy.get(".game-tile").should("be.visible");
-        cy.get(".game-tile").should("have.length", 9);
-    }
-    )
-})
+  it("Loads the page", () => {
+    cy.visit("/");
+    cy.get(".game-tile").should("be.visible");
+    cy.get(".game-tile").should("have.length", 9);
+  });
+});
