307f4381b4f70266febdd7848f8ce1d86b01f3d0
Slin Lee <slinlee@gmail.com>
2022-02-20 13:39:23 -0800

    Add more basic tests for empty and completed wallet

--- cypress/integration/check_dots.js
+++ cypress/integration/check_dots.js
@@ -6,9 +6,22 @@
 // https://on.cypress.io/writing-first-test
 
 describe("Dots Tests", () => {
-  it("Loads the page", () => {
+  it("load the default page", () => {
     cy.visit("/");
     cy.get(".game-tile").should("be.visible");
     cy.get(".game-tile").should("have.length", 9);
+    cy.get(".game-tile.completed").should("have.length", 0);
+  });
+
+  xit("loads a wallet that is complete", () => {
+    cy.visit("/?wallet=0x2A9d8CfD86796E6A68AF9c83FD90F67CcaF1352c");
+    cy.get(".game-tile.completed").should("have.length", 9);
+  });
+
+  it("load a wallet that has not completed anything", () => {
+    cy.visit("/");
+    cy.get(".game-tile").should("be.visible");
+    cy.get(".game-tile").should("have.length", 9);
+    cy.get(".game-tile.completed").should("have.length", 0);
   });
 });
