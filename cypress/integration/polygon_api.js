f181985e7e93118024b09a3c8885f45337edf430
Slin Lee <slinlee@gmail.com>
2022-02-22 16:57:10 -0800

    Format with prettier

--- cypress/integration/polygon_api.js
+++ cypress/integration/polygon_api.js
@@ -1,13 +1,25 @@
-describe('Polygon endpoint tests', () => {
-  it('should fail without a wallet', () => {
-    cy.request({url:'/api/polygon', failOnStatusCode: false}).then((response) => {
-      expect(response.status).to.equal(500)
-    })
-  })
+describe("Polygon endpoint tests", () => {
+  it("should fail without a wallet", () => {
+    cy.request({ url: "/api/polygon", failOnStatusCode: false }).then(
+      (response) => {
+        expect(response.status).to.equal(500);
+      }
+    );
+  });
 
-  it('should return data with a wallet', () => {
-    cy.request('/api/polygon/?wallet_address=test').then((response) => {
-      expect(response.status).to.equal(200)
-    })
-  })
-})
+  it("should return data with a wallet", () => {
+    cy.request(
+      "/api/polygon/?wallet_address=0x2A9d8CfD86796E6A68AF9c83FD90F67CcaF1352c"
+    ).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+
+  it("should handle an invalid wallet", () => {
+    cy.request({ url: "/api/polygon", failOnStatusCode: false }).then(
+      (response) => {
+        expect(response.status).to.equal(500);
+      }
+    );
+  });
+});
