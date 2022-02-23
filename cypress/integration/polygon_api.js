b15dd03cc11ae8905293354783d9c197a5ff5a6e
Slin Lee <slinlee@gmail.com>
2022-02-22 20:45:09 -0800

    cp

--- cypress/integration/polygon_api.js
+++ cypress/integration/polygon_api.js
@@ -1,27 +1,39 @@
-describe.only("Polygon endpoint tests", () => {
-  it("should fail without a wallet", () => {
-    cy.request({ url: "/api/polygon", failOnStatusCode: false }).then(
+describe('Polygon endpoint tests', () => {
+  it('should fail without a wallet', () => {
+    cy.request({ url: '/api/polygon', failOnStatusCode: false }).then(
       (response) => {
-        expect(response.status).to.equal(500);
-      }
-    );
-  });
+        expect(response.status).to.equal(500)
+      },
+    )
+  })
 
-  it("should return data with a wallet", () => {
+  it('should return data with a wallet', () => {
+    // Example: {"tokenBalance":"31331000000000000000000","hasUsedFaucet":true,"hasSentTokens":true,"hasMintedNFT":true,"hasBurnedTokens":true,"hasSwappedTokens":true,"hasVotedInPoll":true,"hasDeployedContract":true}
+    const expected = {
+      tokenBalance: '31331000000000000000000',
+      hasUsedFaucet: true,
+      hasSentTokens: true,
+      hasMintedNFT: true,
+      hasBurnedTokens: true,
+      hasSwappedTokens: true,
+      hasVotedInPoll: true,
+      hasDeployedContract: true,
+    }
     cy.request(
-      "/api/polygon/?wallet_address=0x2A9d8CfD86796E6A68AF9c83FD90F67CcaF1352c"
+      '/api/polygon/?wallet_address=0x2A9d8CfD86796E6A68AF9c83FD90F67CcaF1352c',
     ).then((response) => {
-      expect(response.status).to.equal(200);
-      expect(response.body).to.equal({}); // debug
-    });
-  });
+      expect(response.status).to.equal(200)
+      expect(response.body).to.deep.equal(expected)
+    })
+  })
 
-  it("should handle an invalid wallet", () => {
+  it('should handle an invalid wallet', () => {
+    // Example: {"tokenBalance":"Error! Invalid address format","hasUsedFaucet":false,"hasSentTokens":false,"hasMintedNFT":false,"hasBurnedTokens":false,"hasSwappedTokens":false,"hasVotedInPoll":false,"hasDeployedContract":false}
     cy.request({
-      url: "/api/polygon/?wallet_address=invalid",
+      url: '/api/polygon/?wallet_address=invalid',
       failOnStatusCode: false,
     }).then((response) => {
-      expect(response.status).to.equal(500);
-    });
-  });
-});
+      expect(response.status).to.equal(400)
+    })
+  })
+})
--- pages/api/polygon.ts
+++ pages/api/polygon.ts
@@ -1,129 +1,131 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  if (req.query.wallet_address === "undefined") {
+  if (req.query.wallet_address === undefined) {
     return res.status(500).json({
-      message: "Wallet address undefined",
-    });
+      message: 'Wallet address undefined',
+    })
   }
+  let tokenBalance:number = 0
+  let hasUsedFaucet:boolean = false
+  let hasSwappedTokens:boolean = false
+  let hasVotedInPoll:boolean = false
+  let hasDeployedContract:boolean = false
+  let hasSentTokens:boolean = false
+  let hasBurnedTokens:boolean = false
+  let hasMintedNFT:boolean = false
 
   const responseTokenBalance = await fetch(
-    "https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x4a14ac36667b574b08443a15093e417db909d7a3&address=" +
+    'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x4a14ac36667b574b08443a15093e417db909d7a3&address=' +
       req.query.wallet_address +
-      "&tag=latest&apikey=" +
-      process.env.POLYGON_API_KEY
-  );
-  const balanceJson = await responseTokenBalance.json();
+      '&tag=latest&apikey=' +
+      process.env.POLYGON_API_KEY,
+  )
+  const balanceJson = await responseTokenBalance.json()
 
-  let tokenBalance = 0;
-
-  tokenBalance = balanceJson.result;
+  if(balanceJson.status === '0' && balanceJson.result.includes('Invalid address')) {
+    return res.status(400).json({
+      message: 'Wallet address invalid',
+    })
+  }
+    
+  tokenBalance = balanceJson.result
 
   const response = await fetch(
-    "https://api.polygonscan.com/api?module=account&action=txlist&address=" +
+    'https://api.polygonscan.com/api?module=account&action=txlist&address=' +
       req.query.wallet_address +
-      "&startblock=0&endblock=99999999&sort=asc&apikey=" +
-      process.env.POLYGON_API_KEY
-  );
-  const json = await response.json();
-
-  let hasUsedFaucet = false;
-  let hasSwappedTokens = false;
-  let hasVotedInPoll = false;
-  let hasDeployedContract = false;
+      '&startblock=0&endblock=99999999&sort=asc&apikey=' +
+      process.env.POLYGON_API_KEY,
+  )
+  const json = await response.json()
 
   for (let i = 0; i < json.result.length; i++) {
-    let transaction = json.result[i];
+    let transaction = json.result[i]
     if (
-      transaction["to"] === "0x67806adca0fd8825da9cddc69b9ba8837a64874b" &&
-      transaction["isError"] === "0"
+      transaction['to'] === '0x67806adca0fd8825da9cddc69b9ba8837a64874b' &&
+      transaction['isError'] === '0'
     ) {
-      hasUsedFaucet = true;
+      hasUsedFaucet = true
     }
 
-    if (transaction["to"] === "" && transaction["isError"] === "0") {
-      hasDeployedContract = true;
+    if (transaction['to'] === '' && transaction['isError'] === '0') {
+      hasDeployedContract = true
     }
 
     if (
-      transaction["to"] ===
-        "0x718ad63821a6a3611Ceb706f15971ee029812365".toLowerCase() &&
-      transaction["isError"] === "0"
+      transaction['to'] ===
+        '0x718ad63821a6a3611Ceb706f15971ee029812365'.toLowerCase() &&
+      transaction['isError'] === '0'
     ) {
-      hasVotedInPoll = true;
+      hasVotedInPoll = true
     }
   }
 
   const internalTxnResponse = await fetch(
-    "https://api.polygonscan.com/api?module=account&action=txlistinternal&address=" +
+    'https://api.polygonscan.com/api?module=account&action=txlistinternal&address=' +
       req.query.wallet_address +
-      "&startblock=0&endblock=99999999&sort=asc&apikey=" +
-      process.env.POLYGON_API_KEY
-  );
-  const internalTxnJson = await internalTxnResponse.json();
+      '&startblock=0&endblock=99999999&sort=asc&apikey=' +
+      process.env.POLYGON_API_KEY,
+  )
+  const internalTxnJson = await internalTxnResponse.json()
 
   for (let i = 0; i < internalTxnJson.result.length; i++) {
-    let transaction = internalTxnJson.result[i];
+    let transaction = internalTxnJson.result[i]
     if (
-      transaction["from"] === "0x67806adca0fd8825da9cddc69b9ba8837a64874b" &&
-      transaction["isError"] === "0"
+      transaction['from'] === '0x67806adca0fd8825da9cddc69b9ba8837a64874b' &&
+      transaction['isError'] === '0'
     ) {
-      hasUsedFaucet = true;
+      hasUsedFaucet = true
     }
 
     if (
-      transaction["from"] === "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45" &&
-      transaction["isError"] === "0"
+      transaction['from'] === '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45' &&
+      transaction['isError'] === '0'
     ) {
-      hasSwappedTokens = true;
+      hasSwappedTokens = true
     }
   }
 
   const erc20response = await fetch(
-    "https://api.polygonscan.com/api?module=account&action=tokentx&contractaddress=0x4a14ac36667b574b08443a15093e417db909d7a3&address=" +
+    'https://api.polygonscan.com/api?module=account&action=tokentx&contractaddress=0x4a14ac36667b574b08443a15093e417db909d7a3&address=' +
       req.query.wallet_address +
-      "&startblock=0&endblock=99999999&sort=asc&apikey=" +
-      process.env.POLYGON_API_KEY
-  );
-  const erc20json = await erc20response.json();
-
-  let hasSentTokens = false;
-  let hasBurnedTokens = false;
+      '&startblock=0&endblock=99999999&sort=asc&apikey=' +
+      process.env.POLYGON_API_KEY,
+  )
+  const erc20json = await erc20response.json()
 
   for (let i = 0; i < erc20json.result.length; i++) {
-    let transaction = erc20json.result[i];
+    let transaction = erc20json.result[i]
     if (
-      transaction["from"] === req.query.wallet_address.toLowerCase() &&
-      transaction["value"] !== undefined &&
-      parseInt(transaction["value"]) >= 100 * 10 ** 18
+      transaction['from'] === req.query.wallet_address.toLowerCase() &&
+      transaction['value'] !== undefined &&
+      parseInt(transaction['value']) >= 100 * 10 ** 18
     ) {
-      hasSentTokens = true;
+      hasSentTokens = true
     }
 
     if (
-      transaction["from"] === req.query.wallet_address.toLowerCase() &&
-      transaction["to"] === "0x000000000000000000000000000000000000dead" &&
-      transaction["value"] !== undefined &&
-      parseInt(transaction["value"]) > 0
+      transaction['from'] === req.query.wallet_address.toLowerCase() &&
+      transaction['to'] === '0x000000000000000000000000000000000000dead' &&
+      transaction['value'] !== undefined &&
+      parseInt(transaction['value']) > 0
     ) {
-      hasBurnedTokens = true;
+      hasBurnedTokens = true
     }
   }
 
   const nftResponse = await fetch(
-    "https://api.polygonscan.com/api?module=account&action=tokennfttx&contractaddress=0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B&address=" +
+    'https://api.polygonscan.com/api?module=account&action=tokennfttx&contractaddress=0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B&address=' +
       req.query.wallet_address +
-      "&startblock=0&endblock=99999999&page=1&offset=100&sort=asc&apikey=" +
-      process.env.POLYGON_API_KEY
-  );
-  const nftJson = await nftResponse.json();
-
-  let hasMintedNFT = false;
+      '&startblock=0&endblock=99999999&page=1&offset=100&sort=asc&apikey=' +
+      process.env.POLYGON_API_KEY,
+  )
+  const nftJson = await nftResponse.json()
 
   for (let i = 0; i < nftJson.result.length; i++) {
-    let transaction = nftJson.result[i];
-    if (transaction["from"] === "0x0000000000000000000000000000000000000000") {
-      hasMintedNFT = true;
+    let transaction = nftJson.result[i]
+    if (transaction['from'] === '0x0000000000000000000000000000000000000000') {
+      hasMintedNFT = true
     }
   }
 
@@ -136,5 +138,5 @@ export default async function handler(req, res) {
     hasSwappedTokens: hasSwappedTokens,
     hasVotedInPoll: hasVotedInPoll,
     hasDeployedContract: hasDeployedContract,
-  });
+  })
 }
