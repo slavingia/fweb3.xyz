37f3cd460aa0aec33ba26476d3f50881b63877c8
Jovan Jester <jovan@jester.cafe>
2022-02-23 00:18:00 -0600

    added ProgressBar component

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -8,7 +8,7 @@ const ProgressBar = ({ progress, showLabel, appendLabelText }: ProgressBarProps)
     return (
         <div className="progress-container">
             <div className="progress-inner" style={{ width: `${progress}%` }}></div>
-            {showLabel ? <span className="progress-label">{progress}{appendLabelText}</span> : ''}
+            {showLabel && <span className="progress-label">{progress}{appendLabelText}</span>}
         </div>
     );
 };
