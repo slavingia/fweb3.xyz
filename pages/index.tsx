ce4a4b7cf6347a6689026990ddedd23d9c77cc70
Slin Lee <slinlee@gmail.com>
2022-02-25 13:42:27 -0800

    Apply Prettier to all files.

--- pages/index.tsx
+++ pages/index.tsx
@@ -154,9 +154,9 @@ const orderedDots = Object.keys(dotContent).reduce((list, key) => {
 }, []);
 
 type DotProps = DotContent & {
-  gameTileCompletionStates: number[],
-  activeDot: number,
-  setActiveDot: (dot: number) => void,
+  gameTileCompletionStates: number[];
+  activeDot: number;
+  setActiveDot: (dot: number) => void;
 };
 
 const Dot: React.FC<DotProps> = ({
@@ -331,24 +331,51 @@ export default function Home() {
           )}
           {activeDot === 0 && (
             <>
-              <p>To get started, click the pulsing button, install MetaMask if you haven&apos;t already, and login.</p>
-              <p>If you&apos;re new, I would recommend taking your seed phrase and sticking it in Apple Notes. It&apos;s good enough for now.</p>
+              <p>
+                To get started, click the pulsing button, install MetaMask if
+                you haven&apos;t already, and login.
+              </p>
+              <p>
+                If you&apos;re new, I would recommend taking your seed phrase
+                and sticking it in Apple Notes. It&apos;s good enough for now.
+              </p>
             </>
           )}
           {activeDot === 1 && (
             <>
               <h2>Receive tokens (for free!)</h2>
-              <p><a href="https://discord.gg/azzGB8MJB2">Join our Discord</a> and ask in #token-requests for 222 $FWEB3 tokens.</p>
-              <p>Please specify your wallet address when you do so! That&apos;s enough to complete all the tasks in the game.</p>
-              <p>Once you receive them, use the #collabland-join channel to verify your ownership and see the rest of the channels on Discord.</p>
+              <p>
+                <a href="https://discord.gg/azzGB8MJB2">Join our Discord</a> and
+                ask in #token-requests for 222 $FWEB3 tokens.
+              </p>
+              <p>
+                Please specify your wallet address when you do so! That&apos;s
+                enough to complete all the tasks in the game.
+              </p>
+              <p>
+                Once you receive them, use the #collabland-join channel to
+                verify your ownership and see the rest of the channels on
+                Discord.
+              </p>
             </>
           )}
           {activeDot === 2 && (
             <>
               <h2>Receive gas using tokens (for free!)</h2>
-              <p>Use our faucet to receive .1 MATIC. You&apos;ll need at least 100 $FWEB3 tokens in order to do this.</p>
-              <p style={{color: "#f55"}}>Note that using the faucet directly will cost some gas, which you don&apos;t have yet! So...</p>
-              <p>Use this website we built to use it for free: <a href="https://fweb3-matic-faucet.netlify.app/">https://fweb3-matic-faucet.netlify.app/</a></p>
+              <p>
+                Use our faucet to receive .1 MATIC. You&apos;ll need at least
+                100 $FWEB3 tokens in order to do this.
+              </p>
+              <p style={{ color: "#f55" }}>
+                Note that using the faucet directly will cost some gas, which
+                you don&apos;t have yet! So...
+              </p>
+              <p>
+                Use this website we built to use it for free:{" "}
+                <a href="https://fweb3-matic-faucet.netlify.app/">
+                  https://fweb3-matic-faucet.netlify.app/
+                </a>
+              </p>
             </>
           )}
           {activeDot === 3 && (
@@ -360,45 +387,100 @@ export default function Home() {
           {activeDot === 4 && (
             <>
               <h2>Mint an NFT</h2>
-              <p>Go to <a href="https://polygonscan.com/address/0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B#writeContract">our diamond NFT smart contract</a> and mint yourself a Diamond NFT that will last forever.</p>
-              <p>To mint yourself a unique diamond, pick a number of your choice and enter it in the “mint” function.</p>
-              <p style={{color: "#f55"}}>If the gas is too high, it means that diamond is already taken. Please try a new one!</p>
-              <p>This will show up in your OpenSea shortly, which you can see here: <a href="https://opensea.io/account">https://opensea.io/account</a></p>
+              <p>
+                Go to{" "}
+                <a href="https://polygonscan.com/address/0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B#writeContract">
+                  our diamond NFT smart contract
+                </a>{" "}
+                and mint yourself a Diamond NFT that will last forever.
+              </p>
+              <p>
+                To mint yourself a unique diamond, pick a number of your choice
+                and enter it in the “mint” function.
+              </p>
+              <p style={{ color: "#f55" }}>
+                If the gas is too high, it means that diamond is already taken.
+                Please try a new one!
+              </p>
+              <p>
+                This will show up in your OpenSea shortly, which you can see
+                here:{" "}
+                <a href="https://opensea.io/account">
+                  https://opensea.io/account
+                </a>
+              </p>
             </>
           )}
           {activeDot === 5 && (
             <>
               <h2>Burn a token</h2>
-              <p>Do this by sending at least 1 $FWEB3 token to: <pre>0x000000000000000000000000000000000000dead</pre></p>
-              <p>This is kind of like throwing a dollar bill in a river. It won&apos;t be reflected in the totalSupply function universally, but there is a paper trail that you effectively destroyed one token. Deflation!</p>
+              <p>
+                Do this by sending at least 1 $FWEB3 token to:{" "}
+                <pre>0x000000000000000000000000000000000000dead</pre>
+              </p>
+              <p>
+                This is kind of like throwing a dollar bill in a river. It
+                won&apos;t be reflected in the totalSupply function universally,
+                but there is a paper trail that you effectively destroyed one
+                token. Deflation!
+              </p>
             </>
           )}
           {activeDot === 6 && (
             <>
               <h2>Swap a token</h2>
-              <p>Go to Uniswap to swap 1 $FWEB3 token for some more MATIC: <a href="https://app.uniswap.org/#/swap?chain=polygon">https://app.uniswap.org/#/swap?chain=polygon</a>.</p>
-              <p>If it doesn&apos;t appear, that means you need to import the FWEB3 token into Uniswap.</p>
+              <p>
+                Go to Uniswap to swap 1 $FWEB3 token for some more MATIC:{" "}
+                <a href="https://app.uniswap.org/#/swap?chain=polygon">
+                  https://app.uniswap.org/#/swap?chain=polygon
+                </a>
+                .
+              </p>
+              <p>
+                If it doesn&apos;t appear, that means you need to import the
+                FWEB3 token into Uniswap.
+              </p>
             </>
           )}
           {activeDot === 7 && (
             <>
               <h2>Vote in a proposal with your tokens</h2>
-              <p>Use our poll contract to vote yes or no. You&apos;ll need at least 100 $FWEB3 tokens in order to do this.</p>
-              <p><a href="https://polygonscan.com/address/0x718ad63821a6a3611Ceb706f15971ee029812365#writeContract">https://polygonscan.com/address/0x718ad63821a6a3611Ceb706f15971ee029812365#writeContract</a></p>
+              <p>
+                Use our poll contract to vote yes or no. You&apos;ll need at
+                least 100 $FWEB3 tokens in order to do this.
+              </p>
+              <p>
+                <a href="https://polygonscan.com/address/0x718ad63821a6a3611Ceb706f15971ee029812365#writeContract">
+                  https://polygonscan.com/address/0x718ad63821a6a3611Ceb706f15971ee029812365#writeContract
+                </a>
+              </p>
               <p>What question are you answering? Who knows!</p>
             </>
           )}
           {activeDot === 8 && (
             <>
               <h2>Create your own token</h2>
-              <p>This is the final step. You’re going to deploy your own code to the Polygon blockchain, just like we had to do to make this game.</p>
-              <p>So far, you have interfaced with **three** smart contracts we have deployed:</p>
+              <p>
+                This is the final step. You’re going to deploy your own code to
+                the Polygon blockchain, just like we had to do to make this
+                game.
+              </p>
+              <p>
+                So far, you have interfaced with **three** smart contracts we
+                have deployed:
+              </p>
               <ol>
                 <li>The ERC20 token for the 10,000,000 FWEB3 tokens</li>
                 <li>The ERC721 token for the Diamond NFT</li>
                 <li>The scratch-made smart contract of the poll above</li>
               </ol>
-              <p>Now you will deploy one of your own. Need help? Check out <a href="https://www.notion.so/s-h-l/Walkthrough-058a7ba0a8fe4d798370e4f6a5fda8b0#669ca4319ed646c683f4098e71505ead">this video</a> we made.</p>
+              <p>
+                Now you will deploy one of your own. Need help? Check out{" "}
+                <a href="https://www.notion.so/s-h-l/Walkthrough-058a7ba0a8fe4d798370e4f6a5fda8b0#669ca4319ed646c683f4098e71505ead">
+                  this video
+                </a>{" "}
+                we made.
+              </p>
             </>
           )}
           {!gameTileCompletionStates[0] && !query.wallet && (
@@ -418,7 +500,7 @@ export default function Home() {
               )}
             </div>
           )}
-          {(completedTiles === 9 && !query.wallet && activeDot == -1) && (
+          {completedTiles === 9 && !query.wallet && activeDot == -1 && (
             <div>
               <p>
                 <strong style={{ color: "white" }}>
