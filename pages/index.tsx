1b094dff4e8ad5750a22acd6d319d7bfdb777ba3
Sahil Lavingia <sahil.lavingia@gmail.com>
2022-02-25 13:10:28 -0800

    cp

--- pages/index.tsx
+++ pages/index.tsx
@@ -360,9 +360,9 @@ export default function Home() {
           {activeDot === 4 && (
             <>
               <h2>Mint an NFT</h2>
-              <p>Go to our NFT smart contract and mint yourself a Diamond NFT that will last forever:</p>
-              <p><a href="https://polygonscan.com/address/0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B#writeContract">https://polygonscan.com/address/0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B#writeContract</a></p>
+              <p>Go to <a href="https://polygonscan.com/address/0x9a323979dD8AebC6ecc156d965C417D39Eb61a5B#writeContract">our diamond NFT smart contract</a> and mint yourself a Diamond NFT that will last forever.</p>
               <p>To mint yourself a unique diamond, pick a number of your choice and enter it in the “mint” function.</p>
+              <p style={{color: "#f55"}}>If the gas is too high, it means that diamond is already taken. Please try a new one!</p>
               <p>This will show up in your OpenSea shortly, which you can see here: <a href="https://opensea.io/account">https://opensea.io/account</a></p>
             </>
           )}
