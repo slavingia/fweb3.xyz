4e98747b0cfedeea9b27c56ab1bcc7f0e23f6cfc
Sharang Dashputre <sharang.d@gmail.com>
2022-02-27 22:04:31 +0400

    Make the hook executable

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,8 +4,11 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
+//
+// const buildPrettierCommand = (filenames) =>
+//   `prettier --write #{filenames}`;
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{js,css,md,json,jsx,ts,tsx}'': ["prettier --write"],
+  // '*.{js,css,md,json,jsx,ts,tsx}'': [buildPrettierCommand],
 };
