b3cc54696560bd52c53082030b3976d2446c9973
Richy Chen <richy@richys-macbook.myfiosgateway.com>
2022-02-27 00:35:07 -0500

    added trophy color to debug

--- pages/api/polygon.ts
+++ pages/api/polygon.ts
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       hasVotedInPoll: parseInt(dots[6]),
       hasDeployedContract: parseInt(dots[7]),
       hasWonGame: parseInt(dots[8]),
+      trophyColor: dots[9],
     });
   }
 
