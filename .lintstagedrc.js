a5c32979831fff8c1b0a094cc79cef5515e1ea38
Sharang Dashputre <sharang.d@gmail.com>
2022-02-27 22:09:15 +0400

    Add back prettier hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,11 +4,11 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
-//
-// const buildPrettierCommand = (filenames) =>
-//   `prettier --write #{filenames}`;
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames.join(" ")}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  // '*.{js,css,md,json,jsx,ts,tsx}'': [buildPrettierCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,css,md,json,jsx,ts,tsx}": [buildPrettierCommand],
 };
