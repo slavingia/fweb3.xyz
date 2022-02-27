ce0ab6edfd9e5cfaa4c675c69b1cef4db6df8709
Sharang Dashputre <sharang.d@gmail.com>
2022-02-27 22:17:07 +0400

    Test prettier hook 2

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
+    .map((f) => `'${path.relative(process.cwd(), f)}'`)
     .join(" --file ")}`;
 
 const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames.join(" ")}`;
+  filenames.map((filename) => `prettier --write '${filename}'`);
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
