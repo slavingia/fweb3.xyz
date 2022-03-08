c0e3386d32d69bbe2e4dc1dc6575a5f03ef50d61
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-03-08 12:16:22 -0800

    add jest coverage and related action

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  coverageReporters: ["json-summary"],
+  roots: ["<rootDir>/pages", "<rootDir>/hooks", "<rootDir>/components"],
 };
 
 module.exports = createJestConfig(customJestConfig);
