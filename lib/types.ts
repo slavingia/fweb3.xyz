2f28fed1e1aefc59eabffcec080e223d4f7c86ad
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-03-11 19:02:06 -0800

    WIP interfaces

--- lib/types.ts
+++ lib/types.ts
@@ -36,3 +36,45 @@ export interface ITileStates {
   completedTiles: number;
   gameTileCompletionStates: number[];
 }
+// {
+
+//   "tokenID": "666",
+//   "tokenName": "Fweb3 Diamond NFT",
+//   "tokenSymbol": "FWEB3DIAMONDNFT",
+//   "tokenDecimal": "0",
+//   "transactionIndex": "79",
+//   "gas": "103743",
+//   "gasPrice": "30069785502",
+//   "gasUsed": "69162",
+//   "cumulativeGasUsed": "9506477",
+//   "input": "deprecated",
+//   "confirmations": "678701"
+// }
+
+export interface IPolygonAPIResponse extends IPolygonCoreResponse {}
+
+// erc20 ex
+export interface IPolygonCoreResponse {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  from: string;
+  contractAddress: string;
+  to: string;
+  value: string;
+  tokenName: string;
+  tokenSymbol: string;
+  tokenDecimal: string;
+  transactionIndex: string;
+  gas: string;
+  gasPrice: string;
+  cumulativeGasUsed: string;
+  input: string;
+  confirmations: string;
+}
+
+interface INFTsTxResponse {
+  tokenID: string;
+}
