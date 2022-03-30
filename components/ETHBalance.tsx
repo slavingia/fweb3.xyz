77f81c2a70f6621fb27bb48e518c555e51cca3d9
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-03-30 07:39:43 -0700

    fix type mismatch

--- components/ETHBalance.tsx
+++ components/ETHBalance.tsx
@@ -7,7 +7,7 @@ const ETHBalance = () => {
   const { account } = useWeb3React<Web3Provider>();
   const { data } = useETHBalance(account);
 
-  return <p>Balance: Ξ{parseBalance(data ?? 0)}</p>;
+  return <p>Balance: Ξ{parseBalance(data.toString() ?? "0")}</p>;
 };
 
 export default ETHBalance;
